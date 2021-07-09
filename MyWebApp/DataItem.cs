ca20d3b698e4e7a754f1ed17f2bc87cf138539c8
User <User@WINDEV2104EVAL>
2021-07-09 00:50:21 -0700

    Add project files.

---
+++ MyWebApp/DataItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApp
+{
+    public class DataItem
+    {
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string Location { get; set; }
+    }
+}
